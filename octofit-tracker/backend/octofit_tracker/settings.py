71ca3b18cf3f248fdc04648482ebc72016b7ed03
JrPatt275 <james@jrpatterson.co.uk>
2025-04-08 19:23:54 +0000

    Update settings and views for development environment configuration

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-(!+2br1d*5((z$@x$gvh=ifduemx5(!w^%ye+i607dxk293yp+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'effective-succotash-jvgrrqg9wxg35qw5-8000.app.github.dev']
 
 
 # Application definition
